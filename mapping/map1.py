fb3d2f2d7ac1178a5a369c510b477db62ad8c24e
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-08-07 17:48:43 -0500

    Add a for loop

--- mapping/map1.py
+++ mapping/map1.py
@@ -2,7 +2,10 @@ import folium
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
+for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+
 
 map.add_child(fg)
 map.save("Map1.html")
