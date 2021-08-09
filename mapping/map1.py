5c2c7d7788e73a5a62801753f151642c33d2ae86
yingzhuo1994 <yingzhuo1994@foxmail.com>
2021-08-09 13:31:29 -0500

    Add and stylize markers

--- mapping/map1.py
+++ mapping/map1.py
@@ -27,7 +27,8 @@ fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(el))))
+    # fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(el))))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=folium.Popup(iframe), fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
 
 map.add_child(fg)
